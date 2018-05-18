7ef089db00726b213178baecf3ca7652c0ed1119
gb-education <systempay@mail.ru>
2018-05-19 01:49:06 +0300

    PHP2 Lesson 1

--- lesson-01/index.php
+++ lesson-01/index.php
@@ -1,43 +1,75 @@
 <?
 
-/**
-* 
-*/
-class Article2
+class Article
 {
 	public $id;
 	public $title;
-	public $content;
-	
-	function __construct($id,$title,$content)
-	{
-		$this->id = $id;
-		$this->title = $title;
-		$this->content = $content;
-	}
+	public $text;
 
-	public function view() {
-		echo "<h1>$this->title</h1> <p>$this->content</p>";
+	function view($title,$text) {
+		echo "<h1>$this->title</h1>\n<p>$this->text</p>\n";
 	}
+	
+}
+
+class Gallery extends Article
+{
+	public $images;
+
+	function __construct($title,$text,$images = '') {
+
+		$this->title = $title;
+		$this->text = $text;
+		$this->images = $images;
+
+		$this->view($title,$text);
+		if ($this->images) {
+			echo "<div>$this->images</div>\n";
+		}
+
+	}
+	
 }
 
 
-$page = new Article2(2,"title","content");
+$title = 'Заголовок статьи';
+$text = 'Содержимое статьи. Текст текстовый.';
+$images = 'Блок изображений';
 
-$page->view();
+$article_01 = new Gallery($title,$text);
+$gallery_01 = new Gallery($title,$text,$images);
 
+
+echo "<br>";
 
 class A {
-	public function foo () {
-		static $x = 0;
-		echo ++ $x;
-	}
+    public function foo() {
+        static $x = 0;
+        echo ++$x;
+    }
 }
-$a1 = new A ();
-$a2 = new A ();
-$a1 -> foo ();
-$a2 -> foo ();
-$a1 -> foo ();
-$a2 -> foo ();$a2 -> foo ();
+$a1 = new A();
+$a2 = new A();
+$a1->foo();
+$a2->foo();
+$a1->foo();
+$a2->foo();
 
-?>
+echo "<br>Вся магия видимо из-за статической переменной Х. При запуске одного и того же класса инкремент происходит при каждом вызове.<br><br>";
+
+class C {
+    public function foo() {
+        static $x = 0;
+        echo ++$x;
+    }
+}
+class B extends C {
+}
+$a1 = new C();
+$b1 = new B();
+$a1->foo(); 
+$b1->foo(); 
+$a1->foo(); 
+$b1->foo();
+
+echo "<br>Тут инкремента в наследнике нет. Поэтому инкремент происходит только в родителе.<br>Сходу сообразить при просомтре листинга результат понять не удалось...";
